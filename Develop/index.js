8e036ae16efb9b9013f86f5b9009dab06a23a1f6
Mola Dafa <mola_d@hotmail.com>
2024-03-13 21:37:13 +1100

    project complete

--- Develop/index.js
+++ Develop/index.js
@@ -34,42 +34,42 @@ const questions = [{
 function createReadme({name, description, siteURL, siteImage, wireframe}) {
     return `# ${name}
 
-    ${description} 
-    
-    
-    
-    ## Table of Contents
-    
-    - [Features](#features)
-    - [Link-to-Webpage](#Link-to-Webpage)
-    - [Appearance](#Appearance)
-    - [About-Developer](#About-developer)
-    - [Wireframe](#Wireframe)
-    
-    
-    ## About-developer
-    
-    Hi my name is Molaligne (Mola) Dafa. I am currently taking part in a coding boot camp and this is my Second project. 
-    In it, I have attempted to demonstrate my skills in CSS and HTML.
-    
-    ## Features
-    
-    - Function nav bar.
-    - semantic HTML design.
-    - Accessible design features
-    - 
-      
-    ## Link-to-Webpage
-    - ${siteURL}
-    
-    ## Appearance
-    
-    The following image demonstrates the webpage's appearance
-    ![Screenshot of webpage](${siteImage})
-    
-    ## Wireframe
-    The following image displays a wireframe of my design
-    ![Wireframe of website](${wireframe})`;   
+${description} 
+
+
+
+## Table of Contents
+
+- [Features](#features)
+- [Link-to-Webpage](#Link-to-Webpage)
+- [Appearance](#Appearance)
+- [About-Developer](#About-developer)
+- [Wireframe](#Wireframe)
+
+
+## About-developer
+
+Hi my name is Molaligne (Mola) Dafa. I am currently taking part in a coding boot camp and this is my Second project. 
+In it, I have attempted to demonstrate my skills in CSS and HTML.
+
+## Features
+
+- Function nav bar.
+- semantic HTML design.
+- Accessible design features
+- 
+
+## Link-to-Webpage
+- ${siteURL}
+
+## Appearance
+
+The following image demonstrates the webpage's appearance
+![Screenshot of webpage](${siteImage})
+
+## Wireframe
+The following image displays a wireframe of my design
+![Wireframe of website](${wireframe})`;   
 }
 
 
@@ -80,7 +80,7 @@ function init() {
 .then((answers) => {
             const readMeContent = createReadme(answers);
             fs.writeFile(`README.md`, readMeContent, (err)=>{
-            err ? console.log(err) : Console.log("Successfull written to file")});    
+            err ? console.log(err) : console.log("Successfull written to file")});    
 });
 }
 
